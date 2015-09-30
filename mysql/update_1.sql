d470bf591235d8d8a6d5fc053685a777ffd7618c
yalferov <yalferov@gmail.com>
2015-09-30 21:57:03 +0500

    Исправления по шаблонам

--- mysql/update_1.sql
+++ mysql/update_1.sql
@@ -1,7 +1,8 @@
-CREATE TABLE IF NOT EXISTS `unread_msg` (
+CREATE TABLE IF NOT EXISTS `comment_unread` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ticket_id` int(11) NOT NULL,
-  `user` int(11) NOT NULL,
-  `date` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL,
+  `user_idkey` varchar(255) NOT NULL,
+  `notify_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=cp1251 AUTO_INCREMENT=31 ;
