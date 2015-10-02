7fcd3b4f658224fc4dedcc8b6f9d80ff113c5ea6
yalferov <yalferov@gmail.com>
2015-10-02 23:20:05 +0500

    no message

--- app/controller/_ticket.php
+++ app/controller/_ticket.php
@@ -1,43 +1,41 @@
 <?php
-       global $params, $ticketStatus, $ticketCategory;
-       $itemsOnPage=20;// Количество элементов на странице
-        $computername=$app->Auth->getUserIdKey();
+global $params, $ticketStatus, $ticketCategory;
+$itemsOnPage=20;// Количество элементов на странице
+$computername=$app->Auth->getUserIdKey();
 
-        if($app->Auth->isEngineer()){
+if($app->Auth->isEngineer()){
 
-            $total_rows=Ticket::
-                where_any_is(array(
-                    array('status'=>TICKET_CLOSE),
-                    array('status'=>TICKET_CANCEL)
-                ))->order_by_desc('datetime_add')->count();
-            $obPagination = new Pagination($total_rows,$itemsOnPage);
-            $offset=$obPagination->getOffset(); // Получаем сдвиг
-            $limit=$obPagination->getLimit(); // Получаем лимит
-            $arNav=$obPagination->getLinksArray(); //Получаем массив ссылок
-            $tickets=Ticket::
-                where_any_is(array(
-                    array('status'=>TICKET_CLOSE),
-                    array('status'=>TICKET_CANCEL)
-                ))->order_by_desc('datetime_add')->limit($limit)->offset($offset)->find_many();
-        } else {
-            $tickets=Ticket::where('computer_name',$computername)->order_by_desc('datetime_add')->limit(10)->find_many();    
+    $total_rows=Ticket::
+        where_any_is(array(
+            array('status'=>TICKET_CLOSE),
+            array('status'=>TICKET_CANCEL)
+        ))->order_by_desc('datetime_add')->count();
+    if($total_rows>0){
+        $obPagination = new Pagination($total_rows,$itemsOnPage);
+        $offset=$obPagination->getOffset(); // Получаем сдвиг
+        $limit=$obPagination->getLimit(); // Получаем лимит
+        $arNav=$obPagination->getLinksArray(); //Получаем массив ссылок
+        $tickets=Ticket::
+            where_any_is(array(
+                array('status'=>TICKET_CLOSE),
+                array('status'=>TICKET_CANCEL)
+            ))->order_by_desc('datetime_add')->limit($limit)->offset($offset)->find_many();
+    }
+} else {
+    $tickets=Ticket::where('computer_name',$computername)->order_by_desc('datetime_add')->limit(10)->find_many();
+}
+
+if($tickets) {
+    foreach ($tickets as $ticket) {
+        $ticket->status_text = $ticketStatus[$ticket->status]["text"];
+        $ticket->status_style = $ticketStatus[$ticket->status]["style"];
+        $ticket->category_text = $ticketCategory[$ticket->category];
+        if (strlen($ticket->text) > 150) {
+            $ticket->text = substr($ticket->text, 0, 150) . "...";
         }
-        
-
-         
-        
-
-
-        foreach($tickets as $ticket){
-            $ticket->status_text=$ticketStatus[$ticket->status]["text"];
-            $ticket->status_style=$ticketStatus[$ticket->status]["style"];
-            $ticket->category_text=$ticketCategory[$ticket->category];
-           if(strlen($ticket->text)>150){
-                $ticket->text=substr($ticket->text,0,150)."...";
-            }
-        }
-    
-        $params['tickets']=$tickets;
-        $params['arnav']=$arNav;
-        $app->render('ticketlist.html',$params);
+    }
+}
+$params['tickets']=$tickets;
+$params['arnav']=$arNav;
+$app->render('ticketlist.html',$params);
 ?>
